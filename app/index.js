bb78fd522048fd275cf6fe8041ea823c32184c00
mersed <kahrimanovic_mersed@hotmail.com>
2019-10-23 15:01:02 +0200

    Updating consumer. Using publicly exposed endpoint to trigger the api request.

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ process.env['NODE_CONFIG_DIR'] = __dirname + "/config";
 
 // importing legacy modules
 require('isomorphic-fetch')
+const express = require('express')
 const config = require('config')
 
 // import custom modules
@@ -14,16 +15,31 @@ const User = require('./sequelize/models/User')
 
 // initializing global variables
 const apiKey = config.get('apiKey')
+const app = express()
+const port = 3001
 
 sequelizeConnection.sync({ force: true })
     .then(() => {
-        setInterval(getUser, 3000);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}!`)
+        })
     })
     .catch(error => {
         console.log(error)
     })
 
+app.get('/get-user', async (req, res) => {
+
+    let user = await getUser();
+    if(user) {
+        insertUserIntoDb(user)
+    }
+
+    res.send(user)
+});
+
 const getUser = async () => {
+    let user = null;
     try {
         let response = await fetch('http://api-server:3000/get-user', {
             method: 'post',
@@ -38,13 +54,15 @@ const getUser = async () => {
         const jsonResponse = await response.json();
         
         if(jsonResponse.success) {
-            insertUserIntoDb(jsonResponse.user)
+            user = jsonResponse.user
         }
     } catch(e) {
         console.log(`Request failed with message:`, e.message)
     }
+
+    return user;
 }
 
-const insertUserIntoDb = (user) => {
-    User.create(user)
+const insertUserIntoDb = async (user) => {
+    return await User.create(user)
 }
