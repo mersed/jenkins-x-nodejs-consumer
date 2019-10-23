f89d180783ffb3204ead390d2b8b3191dc4bb8a4
Mersed <kahrimanovic.mersed@gmail.com>
2019-10-23 08:12:18 +0200

    Adding configuration and apiKey

--- app/index.js
+++ app/index.js
@@ -1,12 +1,22 @@
+// Setting up configuration directory
+process.env['NODE_CONFIG_DIR'] = __dirname + "/config";
+
+// importing legacy modules
 require('isomorphic-fetch')
+const config = require('config')
+const apiKey = config.get('apiKey')
 
 const getUser = async () => {
     try {
-        let response = await fetch('http://localhost:3000/get-user', {
+        let response = await fetch('http://api-server:3000/get-user', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
-            }
+            },
+            body: JSON.stringify({
+                // Validation data coming from a form usually
+                apiKey: apiKey
+            })
         });
         const jsonResponse = await response.json();
         console.log(jsonResponse)
